9da3e4eb064adff732ff32dcf589b9f38332419f
fenix <693909080@qq.com>
2025-01-26 00:31:09 +0800

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
+vim.opt.laststatus = 3
+vim.o.cursorline = true
 vim.api.nvim_set_keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<C-z>", "<C-o>u", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,19 @@
+-- === === rainbow，彩虹括号，必须要保证安装了treesitter,我分开是为了方便通过文件查找配置
+-- ===
+return {}
+-- return {
+--   "p00f/nvim-ts-rainbow",
+--   config = function()
+--     require("nvim-treesitter.configs").setup({
+--       rainbow = {
+--         -- `false` will disable the whole extension
+--         enable = true,
+--         -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+--         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+--         max_file_lines = nil, -- Do not enable for files with more than n lines, int
+--         -- colors = {}, -- table of hex strings
+--         -- termcolors = {} -- table of colour name strings
+--       },
+--     })
+--   end,
+-- }
