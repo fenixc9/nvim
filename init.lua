6b2e65ea582f7adffbe8fef999dc0d796f00be0b
agan <agan.lh@alibaba-inc.com>
2025-03-27 20:10:00 +0800

    update

--- init.lua
+++ init.lua
@@ -9,17 +9,17 @@ vim.api.nvim_set_keymap("n", "<C-e>", "<C-u>", { noremap = true, silent = true }
 -- 修改 Go 常量的颜色
 vim.api.nvim_set_hl(0, "goConstant", { fg = "#FFFF00" })
 vim.api.nvim_set_hl(0, "@constant", { fg = "#FFFF00" })
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+-- vim.g.clipboard = {
+--   name = "OSC 52",
+--   copy = {
+--     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+--     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+--   },
+--   paste = {
+--     ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+--     ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+--   },
+-- }
 -- 切换到 Normal 模式时自动切换为英文输入法
 vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = "*",
@@ -35,3 +35,39 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 --     os.execute("rm -rf ~/.local/share/nvim/blink/cmp/fuzzy.db")
 --   end,
 -- })
+--
+-- neovide配置
+if vim.g.neovide then
+  -- vim.g.neovide_input_use_logo = 1
+  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  -- vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  -- vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  -- vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  -- vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  -- vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  -- vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  -- vim.g.neovide_transparency = 0.8
+  -- 性能分析
+  vim.g.neovide_profiler = false
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  -- 全屏模式
+  vim.g.neovide_fullscreen = true
+  -- 强制重绘
+  vim.g.neovide_no_idle = true
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_cursor_animation_length = 0.1
+  vim.g.neovide_cursor_smooth_blink = false
+  -- 粒子特效
+  -- 可设置为 "railgun" "torpedo", "pixiedust", "sonicboom", "ripple", "wireframe", 或空字符串 ""
+  vim.g.neovide_cursor_vfx_mode = ""
+  -- 刷新率
+  vim.g.neovide_refresh_rate = 60
+  --
+  vim.g.neovide_cursor_vfx_particle_curl = 1.0
+  vim.g.neovide_cursor_vfx_particle_speed = 2
+  vim.g.neovide_cursor_antialiasing = true
+end
